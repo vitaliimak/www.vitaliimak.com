7f0e667472bab5152f2462699fb1181342b14400
Vitalii Makarchenkov <mail@vitaliimak.com>
2022-08-28 20:54:03 +0300

    feat: add base style

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const CleanCSS = require("clean-css");
 
 module.exports = function(config) {
 
@@ -11,6 +12,14 @@ module.exports = function(config) {
             day: 'numeric',
         });
     });
+
+  config.addFilter("cssmin", function(code) {
+    if(process.env.NODE_ENV === "production") {
+      return new CleanCSS({}).minify(code).styles;
+    }
+
+    return code;
+  });
 
   config.addCollection('postsReversed', function(collection) {
     return collection.getFilteredByTag('post').reverse();
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [],
+  purge: ['./_site/**/*.{js,html,css,md,svg}'],
 }
