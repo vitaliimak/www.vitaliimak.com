9b5f09c1990ad296c68391efeb3ba366ccfd71d7
Vitalii Makarchenkov <mail@vitaliimak.com>
2022-08-28 05:42:02 +0300

    feat: setup 11ty

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+
+  config.addFilter('readableDate', (value) => {
+        return value.toLocaleString('en', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+    });
+
+  config.addCollection('postsReversed', function(collection) {
+    return collection.getFilteredByTag('post').reverse();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    },
+    passthroughFileCopy: true,
+    templateFormats: [
+            'md',
+            'njk',
+        ],
+  }
+};
