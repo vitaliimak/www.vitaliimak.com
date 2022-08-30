6837e46c23ee19a9cd80b868dccbc052ad85a5a4
Vitalii Makarchenkov <mail@vitaliimak.com>
2022-08-30 17:47:47 +0300

    feat: add target="_blank" attribute to the markdown-it parser for external links https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,27 @@
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const markdownIt = require("markdown-it");
 
 module.exports = function(config) {
   config.addPlugin(rssPlugin);
 
   config.addPassthroughCopy({ 'src/images': 'images' });
+
+  const md = markdownIt();
+  const defaultRender = md.renderer.rules?.link_open || function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    const aIndex = tokens[idx].attrIndex('target');
+
+    if (aIndex < 0) {
+      tokens[idx].attrPush(['target', '_blank']);
+    } else {
+      tokens[idx].attrs[aIndex][1] = '_blank';
+    }
+
+    return defaultRender(tokens, idx, options, env, self);
+  };
+  config.setLibrary("md", md);
 
   return {
     dir: {
