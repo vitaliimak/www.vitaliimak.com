ad931aa193836ceb0757e039b73bb491b006782a
Vitalii Makarchenkov <mail@vitaliimak.com>
2022-08-31 18:36:02 +0300

    feat: add CNAME file to the build

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
   config.addPlugin(syntaxHighlight);
 
   config.addPassthroughCopy({ 'src/images': 'images' });
+  config.addPassthroughCopy('src/CNAME');
 
   const md = markdownIt();
   const defaultRender = md.renderer.rules?.link_open || function(tokens, idx, options, env, self) {
